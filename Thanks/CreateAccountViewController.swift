bad7df776c8a0d07b0499fd460ed01c2fb4ff3bc
Jan Senderek <jan@jan-mbp.corp.dropbox.com>
2016-03-20 18:34:43 -0700

    Onbaording name screen
    Added second onboarding screen

--- Thanks/CreateAccountViewController.swift
+++ Thanks/CreateAccountViewController.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
+class CreateAccountViewController: UIViewController, UITextFieldDelegate {
 
+    
+    //Outlets
+    @IBOutlet weak var nameField: UITextField!
+    
+    
+    //ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Text field delegate to be able to cause actions from keyboard return key
+        self.nameField.delegate = self
+    }
+
+    
+    //ViewDidAppear
+    override func viewDidAppear(animated: Bool) {
+        nameField.becomeFirstResponder()
+    }
+    
+    
+    //Hook up keyboard return key action
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        nameField.resignFirstResponder()
+        nextStep()
+        return true
+    }
+    
+    
+    //The keyboard return key action - we can modify this later to show the input cells for email and password
+    func nextStep() {
+        performSegueWithIdentifier("createAccount", sender: self)
+        //action events
+    }
+    
+    
+    
+    
     
     //Hide status bar
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+
+    
+    //Hide keyboard on tap
+    @IBAction func tappedView(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+
+
+
 }
