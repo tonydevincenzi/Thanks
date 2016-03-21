ed04678b6dadbee9d6a5c2ed15599796444bcb26
Jan Senderek <jan@jan-mbp.corp.dropbox.com>
2016-03-21 09:53:40 -0700

    Radio segmented control for reminders

--- Thanks/RemindersViewController.swift
+++ Thanks/RemindersViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class RemindersViewController: UIViewController {
 
+    
+    //Outlets
+    @IBOutlet weak var dailyButton: UIButton!
+    @IBOutlet weak var monthlyButton: UIButton!
+    @IBOutlet weak var weeklyButton: UIButton!
+    @IBOutlet weak var yearlyButton: UIButton!
+    
+    
+    
+    //ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +36,35 @@ class RemindersViewController: UIViewController {
         return true
     }
 
+    
+    @IBAction func onReminderEditingChanged(sender: AnyObject) {
+        if dailyButton.highlighted == true {
+            dailyButton.selected = true
+            weeklyButton.selected = false
+            monthlyButton.selected = false
+            yearlyButton.selected = false
+        } else if weeklyButton.highlighted == true {
+            dailyButton.selected = false
+            weeklyButton.selected = true
+            monthlyButton.selected = false
+            yearlyButton.selected = false
+        } else if monthlyButton.highlighted == true {
+            dailyButton.selected = false
+            weeklyButton.selected = false
+            monthlyButton.selected = true
+            yearlyButton.selected = false
+        } else if yearlyButton.highlighted == true {
+            dailyButton.selected = false
+            weeklyButton.selected = false
+            monthlyButton.selected = false
+            yearlyButton.selected = true
+        }
+        
+    }
+    
+    
+    
+    
     
     //Dismissed reminder options
     @IBAction func didTapDismiss(sender: AnyObject) {
