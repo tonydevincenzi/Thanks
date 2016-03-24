cf07a0f6d6efb00b6b21d42d2fe6267066bd4844
Anthony DeVincenzi <tonydev@gmail.com>
2016-03-23 19:51:24 -0700

    Updates to settings page

--- Thanks/SettingsViewController.swift
+++ Thanks/SettingsViewController.swift
@@ -12,10 +12,45 @@ import Parse
 class SettingsViewController: UIViewController {
     
     
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    let remindersButton: ThanksButton = ThanksButton()
+    let logoutButton: ThanksButton = ThanksButton()
+    let deleteButton: ThanksButton = ThanksButton()
+    
+    
     override func viewDidLoad() {
+        
+        let parseService: ParseService = ParseService()
+        let currentUser = parseService.getUser()
+        
+        userLabel.text = currentUser.username
+        emailLabel.text = currentUser.email
+        
+        remindersButton.format("Change Reminders", image: nil, tag: 0, xpos: self.view.frame.width/2 - 100, ypos: 220, width: 200, height: 30, shadow: false)
+        
+        remindersButton.setStyleDark()
+        remindersButton.addTarget(self, action: "didTapReminders:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        logoutButton.format("Logout", image: nil, tag: 0, xpos: self.view.frame.width/2 - 100, ypos: 270, width: 200, height: 30, shadow: false)
+        logoutButton.setStyleDark()
+        logoutButton.addTarget(self, action: "didTapLogout:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        deleteButton.format("Delete Account", image: nil, tag: 0, xpos: self.view.frame.width/2 - 100, ypos: 320, width: 200, height: 30, shadow: false)
+        deleteButton.setStyleDark()
+        deleteButton.addTarget(self, action: "didTapDeleteAccount:", forControlEvents: UIControlEvents.TouchUpInside)
+
+        
+        view.addSubview(remindersButton)
+        view.addSubview(logoutButton)
+        view.addSubview(deleteButton)
         
     }
     
+    func didTapReminders(sender: AnyObject) {
+        performSegueWithIdentifier("showReminders", sender: self)
+    }
     
     @IBAction func didTapBack(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
